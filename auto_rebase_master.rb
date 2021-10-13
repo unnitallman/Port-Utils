c27e861b4d8a43f117a53a8b5c8259581ebe73ec
Unnikrishnan KP <unni.tallman@gmail.com>
2021-10-14 00:29:31 +0530

    update

--- auto_rebase_master.rb
+++ auto_rebase_master.rb
@@ -2,5 +2,5 @@ require 'octokit'
 
 puts "Hello world"
 
-client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
+client = Octokit::Client.new(:access_token => ENV["AUTOREBASE_TOKEN"])
 puts client.user.name
